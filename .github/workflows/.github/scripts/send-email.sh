4a2d7c2cf124483ebe829842742e49e06859a468
Jancke Du Toit <janckedt@gmail.com>
2024-08-14 12:49:05 +0200

    Add and update send-email.sh script for notifications

--- .github/workflows/.github/scripts/send-email.sh
+++ .github/workflows/.github/scripts/send-email.sh
@@ -4,7 +4,7 @@ subject=$2
 body=$3
 
 echo -e "Subject:${subject}\n\n${body}" | \
-curl --url 'smtp://smtp.gmail.com:587' --ssl-reqd \
+curl --url "smtp://${SMTP_SERVER}:${SMTP_PORT}" --ssl-reqd \
      --mail-from "${SMTP_USER}" \
      --mail-rcpt "${recipient}" \
      --user "${SMTP_USER}:${SMTP_PASSWORD}" \
